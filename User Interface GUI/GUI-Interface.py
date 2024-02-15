5dcb9cc7ecb29407a8684f264c6fac470f03a3b1
Pysender <159726206+kaliaimailer@users.noreply.github.com>
2024-02-16 04:06:24 +0530

    api

--- User Interface GUI/GUI-Interface.py
+++ User Interface GUI/GUI-Interface.py
@@ -1,11 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
 
 def create_window():
     app = QApplication(sys.argv)
     window = QWidget()
     window.resize(1200, 600)
     window.setWindowTitle('Kali AI Mailer')
+
+    # Set the window icon
+    icon = QIcon("C:/KaliAIMailer/User Interface GUI/kaliaimailer.ico")  
+    window.setWindowIcon(icon)
+
+    # Logo dimensions
+    logo_width = 800  # Adjust the width as needed
+    logo_height = 400  # Adjust the height as needed
+
+    # Logo at the top
+    logo_pixmap = QPixmap("C:/KaliAIMailer/User Interface GUI/Kaliaimailer-logo.jpg")  # Replace with the path to your logo image
+    logo_pixmap = logo_pixmap.scaled(logo_width, logo_height, Qt.KeepAspectRatio)  # Scale the logo
+    logo_label = QLabel(window)
+    logo_label.setPixmap(logo_pixmap)
+    logo_label.setAlignment(Qt.AlignCenter)
+    logo_label.setGeometry(200, 20, logo_width, logo_height)  # Adjust the position and size of the logo
+
     window.show()
     sys.exit(app.exec_())
 
