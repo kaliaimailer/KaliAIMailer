42318171f69f8db047980496ce98d7bbf81819ab
Pysender <159726206+kaliaimailer@users.noreply.github.com>
2024-02-28 02:21:58 +0530

    x

--- randomizer/Main.py
+++ randomizer/Main.py
@@ -3,10 +3,12 @@ from random_alphabet import random_alphabet
 from random_character import random_character
 from random_emoji import random_emoji
 from random_special_character import random_special_character
+from random_capital_alphabet import random_capital_alphabet
 
 def main():
     print("Random Number:", random_number(1, 100))
     print("Random Alphabet:", random_alphabet())
+    print("Random Capital Alphabet:", random_capital_alphabet())
     print("Random Character:", random_character())
     print("Random Special Character:", random_special_character())
     print("Random Emoji:", random_emoji())
---
+++ randomizer/random_capital_alphabet.py
@@ -0,0 +1,5 @@
+import random
+import string
+
+def random_capital_alphabet():
+    return random.choice(string.ascii_uppercase)
